a6d3ebb6f6016ebca69c2fb591f2998ebf99682b
xiaweicai <0049000390@znv.com>
2020-02-04 06:34:51 +0800

    remove questions list form Game.class

--- src/main/java/com/adaptionsoft/games/Game.java
+++ src/main/java/com/adaptionsoft/games/Game.java
@@ -9,26 +9,9 @@ public class Game {
     int[] purses  = new int[6];
     boolean[] inPenaltyBox  = new boolean[6];
 
-    LinkedList popQuestions = new LinkedList();
-    LinkedList scienceQuestions = new LinkedList();
-    LinkedList sportsQuestions = new LinkedList();
-    LinkedList rockQuestions = new LinkedList();
 	private final DecksManager decksManager = new DecksManager();
     int currentPlayer = 0;
     boolean isGettingOutOfPenaltyBox;
-
-    public Game(){
-    	for (int i = 0; i < 50; i++) {
-			popQuestions.addLast("Pop Question " + i);
-			scienceQuestions.addLast(("Science Question " + i));
-			sportsQuestions.addLast(("Sports Question " + i));
-			rockQuestions.addLast(createRockQuestion(i));
-    	}
-    }
-
-	public String createRockQuestion(int index){
-		return "Rock Question " + index;
-	}
 
 	public boolean isPlayable() {
 		return (howManyPlayers() >= 2);
