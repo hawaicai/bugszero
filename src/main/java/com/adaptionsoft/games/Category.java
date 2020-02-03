35e309ad6b4cd5f219f39aee07e2f6c899fab75c
xiaweicai <0049000390@znv.com>
2020-02-03 18:58:08 +0800

    exact Category.class

---
+++ src/main/java/com/adaptionsoft/games/Category.java
@@ -0,0 +1,25 @@
+package com.adaptionsoft.games;
+
+public enum Category {
+    POP("Pop"),
+    SCIENCE("Science"),
+    SPORTS("Sports"),
+    ROCK("Rock");
+
+    private final String name;
+
+    Category(String name) {
+        this.name = name;
+    }
+
+    public static String currentCategory(int places) {
+        Category[] array = Category.values();
+        int size = array.length;
+        return array[places % size].name;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+}
--- src/main/java/com/adaptionsoft/games/QuestionsManager.java
+++ src/main/java/com/adaptionsoft/games/QuestionsManager.java
@@ -1,21 +1,16 @@
 package com.adaptionsoft.games;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
 
 public class QuestionsManager {
-    public static final String POP = "Pop";
-    public static final String SCIENCE = "Science";
-    public static final String SPORTS = "Sports";
-    public static final String ROCK = "Rock";
     private Map<String, Deck> decks = new HashMap<>();
+
     public QuestionsManager()
     {
-        decks.put(POP, new Deck(POP));
-        decks.put(SCIENCE, new Deck(SCIENCE));
-        decks.put(SPORTS,new Deck(SPORTS));
-        decks.put(ROCK, new Deck(ROCK));
+        for (Category category:Category.values()) {
+            decks.put(category.getName(), new Deck(category.getName()));
+        }
     }
 
     public String askQuestion(int places) {
@@ -24,15 +19,6 @@ public class QuestionsManager {
         }
 
     public String currentCategory(int places) {
-        if (places == 0) return POP;
-        if (places == 4) return POP;
-        if (places == 8) return POP;
-        if (places == 1) return SCIENCE;
-        if (places == 5) return SCIENCE;
-        if (places == 9) return SCIENCE;
-        if (places == 2) return SPORTS;
-        if (places == 6) return SPORTS;
-        if (places == 10) return SPORTS;
-        return ROCK;
+        return Category.currentCategory(places);
     }
 }
